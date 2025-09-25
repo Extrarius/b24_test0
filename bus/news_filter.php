f9be0953b45d0981212c2229b2d92c747c93abde
Oleg <142502317+Extrarius@users.noreply.github.com>
2025-09-25 11:52:58 +0300

    Create news_filter.php

---
+++ bus/news_filter.php
@@ -0,0 +1,14 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Фильтр новостей");
+
+// Подключаем компонент
+$APPLICATION->IncludeComponent('custom:news.filter.list', '', [
+  'IBLOCK_ID' => 1,
+  'AJAX_MODE' => 'Y',         // применяет фильтр без полной перезагрузки
+  'AJAX_OPTION_HISTORY' => 'Y',
+  'AJAX_OPTION_STYLE'   => 'Y',
+  'AJAX_OPTION_JUMP'    => 'N',
+]);
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
