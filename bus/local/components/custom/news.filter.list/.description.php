6416ef3a1c48aeb1322a2d5579b92cdec85c6a3f
Oleg <142502317+Extrarius@users.noreply.github.com>
2025-09-25 11:17:53 +0300

    Create .description.php

---
+++ bus/local/components/custom/news.filter.list/.description.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = [
+  "NAME" => "News list with filter",
+  "DESCRIPTION" => "Список элементов ИБ «Новости» + форма фильтра (поиск по названию, COST, период).",
+  "PATH" => [ "ID" => "site_components", "NAME" => "Site components" ],
+];
