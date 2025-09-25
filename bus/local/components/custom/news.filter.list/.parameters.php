556bece022736cd3dcfa6d1b50c508b623dd16ba
Oleg <142502317+Extrarius@users.noreply.github.com>
2025-09-25 11:18:23 +0300

    Create .parameters.php

---
+++ bus/local/components/custom/news.filter.list/.parameters.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentParameters = [
+  "PARAMETERS" => [
+    "IBLOCK_ID" => [ "PARENT"=>"BASE", "NAME"=>"ID инфоблока", "TYPE"=>"STRING", "DEFAULT"=>"" ],
+  ]
+];
